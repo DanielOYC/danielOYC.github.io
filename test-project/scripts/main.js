fd6129f040015860284833f4bc067ef673da5596
Ou Yangcheng <775350532@qq.com>
2023-01-16 17:06:18 +0800

    Delete main.js

--- test-project/scripts/main.js
+++
@@ -1,7 +0,0 @@
-let myHeading = document.querySelector('h1')
-myHeading.textContent = 'Hello world!'
-
-
-document.querySelector("html").addEventListener("click", function () {
-    alert("别戳我，我怕疼。");
-  });
